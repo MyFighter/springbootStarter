3eaf57965b2fa6a8d6fe553511b84c32d14b0c82
nicesky <99783938@qq.com>
2020-06-18 14:00:13 +0800

    change.

---
+++ leetCodeExercises/src/Tree/Test_226.java
@@ -0,0 +1,13 @@
+package Tree;
+
+public class Test_226 {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+        return root;
+    }
+}
