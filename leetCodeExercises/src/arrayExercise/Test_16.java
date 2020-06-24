701dd2463f4e597fa01d48b13c80dffa6f73bb6e
niceky <994783938@qq.com>
2020-06-24 22:54:31 +0800

    Test_16 Changed,uncompleted.

--- leetCodeExercises/src/arrayExercise/Test_16.java
+++ leetCodeExercises/src/arrayExercise/Test_16.java
@@ -2,12 +2,16 @@ package arrayExercise;
 
 public class Test_16 {
 
+//    Input: nums = [-1,2,1,-4], target = 1
+//    Output: 2
     public int threeSumClosest(int[] nums, int target) {
-        int sum = 0;
-        int[] sizeArr = new int[3001];
-        for (int i = 0; i < nums.length ; i++) {
+        int diff = Math.abs(nums[0]-target);
+        for (int i = 1; i < nums.length; i++) {
 
         }
+
+        int sum = 0;
+
         return sum;
     }
 
