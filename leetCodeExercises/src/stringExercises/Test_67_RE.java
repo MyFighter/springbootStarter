8dd30b0fa73d17e086df71d1bad7d076a2947030
nicesky <99783938@qq.com>
2020-06-24 21:09:03 +0800

    Test_16 uncompleted.

---
+++ leetCodeExercises/src/stringExercises/Test_67_RE.java
@@ -0,0 +1,12 @@
+package stringExercises;
+
+public class Test_67_RE {
+//    Input: a = "1010", b = "1011"
+//    Output: "10101"
+    public String addBinary(String a, String b) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+    }
+}
