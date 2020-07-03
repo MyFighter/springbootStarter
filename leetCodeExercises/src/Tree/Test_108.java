dbef67a2e98c92ab343b23d8f02fea11694eb7b7
nicesky <99783938@qq.com>
2020-07-03 17:59:11 +0800

    change.

---
+++ leetCodeExercises/src/Tree/Test_108.java
@@ -0,0 +1,30 @@
+package Tree;
+
+public class Test_108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+        return null;
+    }
+    //1,2,3,4
+    /*
+    *    3
+    *   2 4
+    *  1   5
+    * */
+    //1,2,3
+    /*
+    *       2
+    *      1 3
+    * */
+//  -5,-4,-3,-2,-1,0,1,2,3,4,5
+    /*
+    *                 0
+    *          -3          3
+    *        -5   -1     1   5
+    *         -4 -2       2 4
+    * */
+    public static void main(String[] args) {
+        Test_108 t = new Test_108();
+
+    }
+}
