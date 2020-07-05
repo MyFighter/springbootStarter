68715201e6742b05dec83fa5c49072e05983aebc
nicesky <99783938@qq.com>
2020-07-05 16:52:51 +0800

    change.

---
+++ leetCodeExercises/src/stringExercises/Test_44.java
@@ -0,0 +1,19 @@
+package stringExercises;
+
+public class Test_44 {
+    public boolean isMatch(String s, String p) {
+        int i=0,j=0;
+        while (i<s.length() && j<p.length()) {
+            char sc = s.charAt(i);
+            char pc = p.charAt(i);
+            if (sc != pc) {
+
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
